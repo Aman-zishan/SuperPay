1ff13e54bbd429af9186bbd07854a15f047fe769
lordmike007 <manujasan23@gmail.com>
2023-02-08 21:09:22 +0530

    added superfluid to context, made superfluid utils

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ProvideAuth } from "@arcana/auth-react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Framework } from "@superfluid-finance/sdk-core";
+import DefaultContext from "./utils/globalState";
+import { ProvideAuth } from "@arcana/auth-react";
 import routerConfig from "./router";
-import "./index.css";
 import { arcanaProvider } from "./utils/auth";
+import "./index.css";
 
 const router = createBrowserRouter(routerConfig);
 
+const sf = await Framework.create({
+  chainId: 137,
+  provider: arcanaProvider,
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-     <ProvideAuth provider={arcanaProvider}>
-      <RouterProvider router={router} />
-   </ProvideAuth>
+    <ProvideAuth provider={arcanaProvider}>
+      <DefaultContext.Provider value={sf}>
+        <RouterProvider router={router} />
+      </DefaultContext.Provider>
+    </ProvideAuth>
   </React.StrictMode>
 );
