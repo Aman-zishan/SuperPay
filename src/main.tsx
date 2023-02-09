19cd66a8400fc968756bb3778548e7cba01cdd27
Aman <amanzishan.az@gmail.com>
2023-02-09 16:18:25 +0530

    arcana connector

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,30 @@ import { ProvideAuth } from "@arcana/auth-react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routerConfig from "./router";
 import "./index.css";
-import { arcanaProvider } from "./utils/auth";
+import { arcanaProvider, connector } from "./utils/auth";
+import { polygon, polygonMumbai } from "@wagmi/core/chains";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
 
 const router = createBrowserRouter(routerConfig);
 
+const { chains, provider } = configureChains(
+  [polygon, polygonMumbai],
+  [publicProvider()]
+);
+
+export const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: connector(chains),
+  provider,
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-     <ProvideAuth provider={arcanaProvider}>
-      <RouterProvider router={router} />
-   </ProvideAuth>
+    <WagmiConfig client={wagmiClient}>
+      <ProvideAuth provider={arcanaProvider}>
+        <RouterProvider router={router} />
+      </ProvideAuth>
+    </WagmiConfig>
   </React.StrictMode>
 );
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,6 +1,6 @@
 import { AuthProvider, CHAIN } from "@arcana/auth";
-import { ethers } from "ethers";
-
+import { ArcanaConnector } from "@arcana/auth-wagmi";
+import { polygon, polygonMumbai } from "@wagmi/core/chains";
 const appAddress = import.meta.env.VITE_APP_ID;
 
 export const arcanaProvider = new AuthProvider(`${appAddress}`, {
@@ -12,5 +12,21 @@ export const arcanaProvider = new AuthProvider(`${appAddress}`, {
     // }.matic-testnet.discover.quiknode.pro/${
     //   import.meta.env.VITE_QUICKNODE_TOKEN
     // }/`,
+  },
+});
+
+export const connector = new ArcanaConnector({
+  chains: [polygon, polygonMumbai],
+  options: {
+    chainConfig: {
+      chainId: CHAIN.POLYGON_MUMBAI_TESTNET,
+      // rpcUrl: `https://${
+      //   import.meta.env.VITE_QUICKNODE_CLIENTD
+      // }.matic-testnet.discover.quiknode.pro/${
+      //   import.meta.env.VITE_QUICKNODE_TOKEN
+      // }/`,
+    },
+    appId: `${appAddress}`,
+    alwaysVisible: true, // Defaults to true
   },
 });
