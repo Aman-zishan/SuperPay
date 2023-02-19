ee1365021a6e80a73ebc578b153a6a9afa7aa760
lordmike007 <manujasan23@gmail.com>
2023-02-19 19:33:34 +0530

    moved to quicknode rpc

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -7,10 +7,6 @@ export const arcanaProvider = new AuthProvider(`${appAddress}`, {
   alwaysVisible: true, // defaults to true which is Full UI mode
   chainConfig: {
     chainId: CHAIN.POLYGON_MUMBAI_TESTNET,
-    // rpcUrl: `https://${
-    //   import.meta.env.VITE_QUICKNODE_CLIENTD
-    // }.matic-testnet.discover.quiknode.pro/${
-    //   import.meta.env.VITE_QUICKNODE_TOKEN
-    // }/`,
+    rpcUrl: import.meta.env.VITE_QUICKNODE_URL,
   },
 });
