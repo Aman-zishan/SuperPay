73b2481fd806d8c2d0cc868b4203ad1d895401ac
Aman <amanzishan.az@gmail.com>
2023-02-19 12:22:28 +0530

    build error fix

--- src/utils/superfluidFunctions.ts
+++ src/utils/superfluidFunctions.ts
@@ -1,93 +1,133 @@
-import { ethers } from "ethers"
-import type { Signer } from "ethers"
-import type {Framework} from "@superfluid-finance/sdk-core";
-
+import { ethers } from "ethers";
+import type { Signer } from "ethers";
+import type { Framework } from "@superfluid-finance/sdk-core";
 
 export const asyncWrapper = async (asyncFunction: () => {}, params: Array) => {
-    try {
-        const data = await asyncFunction(...params)
-        return [data, null]
-    }
-    catch (error) {
-        return [ null, error ]
-    }
-}
+  try {
+    const data = await asyncFunction(...params);
+    return [data, null];
+  } catch (error) {
+    return [null, error];
+  }
+};
 
-const startFlow = async (sf:Framework, sender: string, receiver: string, flowRate: string, signer: Signer, userData?: string ) => {
-//load the token you'd like to use like this 
-//note that tokens may be loaded by symbol or by address
-const superToken = await sf?.loadSuperToken("0x93D50cF93EBDa2f06f564339a31E0cac81fa479C");
+const startFlow = async (
+  sf: Framework,
+  sender: string,
+  receiver: string,
+  flowRate: string,
+  signer: Signer,
+  userData?: string
+) => {
+  //load the token you'd like to use like this
+  //note that tokens may be loaded by symbol or by address
+  const superToken = await sf?.loadSuperToken(
+    "0x93D50cF93EBDa2f06f564339a31E0cac81fa479C"
+  );
 
-let flowOp = superToken?.createFlow({
+  let flowOp = superToken?.createFlow({
     sender,
     receiver,
     flowRate,
     userData,
-});
+  });
 
-    await flowOp?.exec( signer ); // should have same address as `sender`
-}
+  await flowOp?.exec(signer); // should have same address as `sender`
+};
 
-const updateFlow = async (sf:Framework, sender: string, receiver: string, flowRate: string, signer: Signer, userData?: string ) => {
-//load the token you'd like to use like this 
-//note that tokens may be loaded by symbol or by address
-const superToken = await sf?.loadSuperToken("DAIx");
+const updateFlow = async (
+  sf: Framework,
+  sender: string,
+  receiver: string,
+  flowRate: string,
+  signer: Signer,
+  userData?: string
+) => {
+  //load the token you'd like to use like this
+  //note that tokens may be loaded by symbol or by address
+  const superToken = await sf?.loadSuperToken("DAIx");
 
-let flowOp = superToken?.createFlow({
-  sender,
-  receiver,
-  flowRate,
-  userData
-});
+  let flowOp = superToken?.createFlow({
+    sender,
+    receiver,
+    flowRate,
+    userData,
+  });
 
-await flowOp?.exec( signer ); // should have same address as `sender`
-}
+  await flowOp?.exec(signer); // should have same address as `sender`
+};
 
-const stopFlow = async (sender: string, receiver: string, userData?: string, signer: Signer) => {
-    //load the token you'd like to use like this 
-    //note that tokens may be loaded by symbol or by address
-    const superToken = await sf?.loadSuperToken("DAIx");
-    
-    let flowOp = superToken?.deleteFlow({
-        sender,
-        receiver,
-        userData
-    });
-    
-    await flowOp?.exec( signer ); // should have same address as `sender`
-}
+const stopFlow = async (
+  sf: Framework,
+  sender: string,
+  receiver: string,
+  userData?: string,
+  signer: Signer
+) => {
+  //load the token you'd like to use like this
+  //note that tokens may be loaded by symbol or by address
+  const superToken = await sf?.loadSuperToken("DAIx");
 
-const approveContractToSpend = async (tokenAddress: string,tokenABI: Array,signer: Signer, amount: string, ) => {
-    const tokenContract = new ethers.Contract(
-        "0x88271d333C72e51516B67f5567c728E702b3eeE8",
-        tokenABI,
-        signer
-      );
-      
-    await tokenContract.approve(
-        tokenAddress,
-        ethers.utils.parseEther(amount)
-    )
-}
+  let flowOp = superToken?.deleteFlow({
+    sender,
+    receiver,
+    userData,
+  });
 
-const upgradeTokens = async (superTokenAddress: string, amount: string, signer: Signer) => {
-    const superToken = await sf?.loadSuperToken(superTokenAddress);
+  await flowOp?.exec(signer); // should have same address as `sender`
+};
 
-    const upgradeOperation = superToken?.upgrade({ amount });
+const approveContractToSpend = async (
+  tokenAddress: string,
+  tokenABI: Array,
+  signer: Signer,
+  amount: string
+) => {
+  const tokenContract = new ethers.Contract(
+    "0x88271d333C72e51516B67f5567c728E702b3eeE8",
+    tokenABI,
+    signer
+  );
 
-    await upgradeOperation.exec(signer);
-}
+  await tokenContract.approve(tokenAddress, ethers.utils.parseEther(amount));
+};
 
-const downgradeTokens = async (superTokenAddress: string, amount: string, signer: Signer) => {
-    const superToken = await sf?.loadSuperToken(superTokenAddress);
+const upgradeTokens = async (
+  sf: Framework,
+  superTokenAddress: string,
+  amount: string,
+  signer: Signer
+) => {
+  const superToken = await sf?.loadSuperToken(superTokenAddress);
 
-    const upgradeOperation = superToken?.downgrade({ amount });
+  const upgradeOperation = superToken?.upgrade({ amount });
 
-    await upgradeOperation.exec(signer);
-}
+  await upgradeOperation.exec(signer);
+};
 
-const batchTxns = async (txnArray: Array, signer: Signer) => {
-    await sf?.batchCall(txnArray).exec(signer)
-}
+const downgradeTokens = async (
+  sf: Framework,
+  superTokenAddress: string,
+  amount: string,
+  signer: Signer
+) => {
+  const superToken = await sf?.loadSuperToken(superTokenAddress);
 
-export {startFlow, updateFlow, stopFlow, approveContractToSpend, upgradeTokens, downgradeTokens, batchTxns};
+  const upgradeOperation = superToken?.downgrade({ amount });
+
+  await upgradeOperation.exec(signer);
+};
+
+const batchTxns = async (sf: Framework, txnArray: Array, signer: Signer) => {
+  await sf?.batchCall(txnArray).exec(signer);
+};
+
+export {
+  startFlow,
+  updateFlow,
+  stopFlow,
+  approveContractToSpend,
+  upgradeTokens,
+  downgradeTokens,
+  batchTxns,
+};
--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "esnext",
+  },
   plugins: [react()],
   server: {
-    port: 3000  ,
+    port: 3000,
   },
 });
